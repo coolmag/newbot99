8966f6798e923db3de3e163447b90e8c45090b41
coolmag <54004066+coolmag@users.noreply.github.com>
2026-02-04 13:42:20 +0300

    Delete static/service-worker.js

--- static/service-worker.js
+++
@@ -1,4 +0,0 @@
-// This is a dummy file to prevent 404 errors from an old, cached index.html.
-// The real player has no service worker.
-// If your browser tries to load this, your deployment is not serving the latest index.html.
-console.error("ERROR: Loading an outdated service-worker.js. Your deployment is not up-to-date.");
